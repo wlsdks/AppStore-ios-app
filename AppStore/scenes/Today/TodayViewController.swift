2cecec6aa04fbc5100c0cc0a83dd5bf3be3749b6
jinan <ja.choi@sweetk.co.kr>
2023-05-20 00:46:11 +0900

    1. collectionView설정 추가 - delegate, datesource추가

--- AppStore/scenes/Today/TodayViewController.swift
+++ AppStore/scenes/Today/TodayViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 final class TodayViewController: UIViewController {
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout() //flowlayout을 초기화 한다.
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
+        
+        return collectionView
+    }()
     
     
     // MARK: - 맨 처음 실행됨
@@ -18,4 +29,24 @@ final class TodayViewController: UIViewController {
         
         view.backgroundColor = .blue
     }
+}
+
+
+extension TodayViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath)
+        cell.backgroundColor = .black
+        
+        return cell
+    }
+    
+}
+
+extension TodayViewController: UICollectionViewDelegate {
+    
 }
