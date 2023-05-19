f3bc8f7999d058472d0b5de0989e6d11431ec9f1
jinan <ja.choi@sweetk.co.kr>
2023-05-20 01:17:41 +0900

    1.TodayCollectionHeaderView 헤더뷰 클래스 추가 및 코드작성

---
+++ AppStore/scenes/Today/TodayCollectionHeaderView.swift
@@ -0,0 +1,46 @@
+//
+//  TodayCollectionHeaderView.swift
+//  AppStore
+//
+//  Created by 최진안 on 2023/05/20.
+//
+
+import UIKit
+import SnapKit
+
+final class TodayCollectionHeaderView: UICollectionReusableView {
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "6월 28일 월요일"
+        label.font = .systemFont(ofSize: 36.0, weight: .black)
+        label.textColor = .label
+        
+        return label
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "투데이"
+        label.font = .systemFont(ofSize: 36.0, weight: .black)
+        label.textColor = .label
+        
+        return label
+    }()
+    
+    func setupViews() {
+        [dateLabel, titleLabel].forEach {
+            addSubview($0)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.trailing.equalToSuperview()
+            $0.top.equalToSuperview().inset(32.0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.left.equalTo(dateLabel)
+            $0.top.equalTo(dateLabel.snp.bottom).offset(8.0)
+        }
+    }
+}
