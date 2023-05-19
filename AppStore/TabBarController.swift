26a3d778287fc22ef7b808d390c13b1aacb2e892
jinan <ja.choi@sweetk.co.kr>
2023-05-20 00:36:02 +0900

    1. 탭바 컨트롤러 생성 및 구현 성공

--- AppStore/TabBarController.swift
+++ AppStore/TabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabBarController.swift
 //  AppStore
 //
 //  Created by 최진안 on 2023/05/18.
@@ -8,11 +8,29 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    private lazy var todayViewController: UIViewController = {
+        let viewController = UIViewController()
+        // 첫번째 탭이라 tag:0이다.
+        let tabBarItem = UITabBarItem(title: "투데이", image: UIImage(systemName: "mail"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    private lazy var appViewController: UIViewController = {
+        let viewController = UIViewController()
+        
+        let tabBarItem = UITabBarItem(title: "앱", image: UIImage(systemName: "square.stack.3d.up"), tag: 1)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        viewControllers = [todayViewController, appViewController]
     }
 
 
