59ce42966430b1e4e49701d1bfb01cab50c1dde9
jinan <ja.choi@sweetk.co.kr>
2023-05-20 01:08:32 +0900

    1. TodayCollectionViewCell 안에서 각 라벨별 코드 추가(layout 설정)

--- AppStore/scenes/Today/TodayCollectionViewCell.swift
+++ AppStore/scenes/Today/TodayCollectionViewCell.swift
@@ -9,7 +9,75 @@ import SnapKit
 import UIKit
 
 final class TodayCollectionViewCell: UICollectionViewCell {
+    
+    private lazy var titleLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var subTitleLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12.0
+        imageView.backgroundColor = .gray
+        
+        return imageView
+    }()
+    
+    
     func setup() {
-        backgroundColor = .black
+        setupSubViews()
+    }
+}
+
+
+private extension TodayCollectionViewCell {
+    func setupSubViews() {
+        [imageView, titleLabel, subTitleLabel, descriptionLabel].forEach {
+            addSubview($0)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.top.equalToSuperview().inset(24.0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(subTitleLabel)
+            $0.trailing.equalTo(subTitleLabel)
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(4.0)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.bottom.equalToSuperview().inset(24.0)
+        }
+        
+        imageView.snp.makeConstraints {
+            // 이미지뷰는 꼭 슈퍼사이지와 같게 해야한다.
+            $0.edges.equalToSuperview()
+        }
     }
 }
