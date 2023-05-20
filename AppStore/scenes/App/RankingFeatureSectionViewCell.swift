de5e33891b811a01c0a9e1250026e484c8190d08
jinan <ja.choi@sweetk.co.kr>
2023-05-20 14:51:46 +0900

    1. rankingSection 내부의 셀에 보여질 5개의 컴포넌트를 RankingFeatureSectionViewCell클래스에 정의한다.

---
+++ AppStore/scenes/App/RankingFeatureSectionViewCell.swift
@@ -0,0 +1,118 @@
+//
+//  RankingFeatureSectionViewCell.swift
+//  AppStore
+//
+//  Created by 최진안 on 2023/05/20.
+//
+
+import UIKit
+import SnapKit
+
+/**
+ rankingSection 내부의 셀에 보여질 5개의 컴포넌트를 정의한다.
+ */
+final class RankingFeatureSectionViewCell: UICollectionViewCell {
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .tertiarySystemGroupedBackground
+        imageView.layer.borderColor = UIColor.tertiaryLabel.cgColor
+        imageView.layer.borderWidth = 0.5
+        imageView.layer.cornerRadius = 7.0
+        
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16.0, weight: .bold)
+        label.textColor = .label
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13.0, weight: .semibold)
+        label.textColor = .secondaryLabel
+        
+        return label
+    }()
+    
+    private lazy var downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("받기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13.0, weight: .bold)
+        button.backgroundColor = .secondarySystemBackground
+        button.layer.cornerRadius = 12.0
+        
+        return button
+    }()
+    
+    private var inAppPurchaseInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "앱 내 구입"
+        label.font = .systemFont(ofSize: 10.0, weight: .semibold)
+        label.textColor = .secondaryLabel
+        
+        return label
+    }()
+    
+    // setup메서드 선언
+    func setup() {
+        setupLayout()
+        
+        titleLabel.text = "앱 이름"
+        descriptionLabel.text = "이런이런 앱입니다."
+        inAppPurchaseInfoLabel.isHidden = [true, false].randomElement() ?? true
+    }
+    
+}
+
+// MARK: - layout을 설정하는 코드 확장 선언
+private extension RankingFeatureSectionViewCell {
+    
+    func setupLayout() {
+        [
+            imageView,
+            titleLabel,
+            descriptionLabel,
+            downloadButton,
+            inAppPurchaseInfoLabel
+        ].forEach{ addSubview($0) }
+        
+        imageView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.equalToSuperview().inset(4.0)
+            $0.bottom.equalToSuperview().inset(4.0)
+            $0.width.equalTo(imageView.snp.height)
+        }
+        
+        downloadButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(24.0)
+            $0.width.equalTo(50.0)
+        }
+        
+        inAppPurchaseInfoLabel.snp.makeConstraints {
+            $0.centerX.equalTo(downloadButton.snp.centerX)
+            $0.top.equalTo(downloadButton.snp.bottom).offset(4.0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(imageView.snp.trailing).offset(8.0)
+            $0.trailing.equalTo(downloadButton.snp.leading)
+            $0.top.equalTo(imageView.snp.top).inset(8.0)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.trailing.equalTo(titleLabel.snp.trailing)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+        }
+    }
+    
+}
