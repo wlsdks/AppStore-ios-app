ce18ecf43bacc06f225609b4d939976fc9f11bf1
jinan <ja.choi@sweetk.co.kr>
2023-05-20 13:18:45 +0900

    1. AppViewController안에 올릴 뷰를 다 선언해주고 하단에 func setupLayout()를 통해 기본 view위에 다 올리도록 코드를 작성

--- AppStore/scenes/App/AppViewController.swift
+++ AppStore/scenes/App/AppViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 final class AppViewController: UIViewController {
     
+    // scrollView안에 contentView가 포함될것이다.
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    // MARK: - stackView 선언
+    private lazy var stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        // stackview의 spacing 처리 (간격 동일)
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,4 +41,34 @@ private extension AppViewController {
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    func setupLayout() {
+        // AppViewController의 가장 기본 view 위에 subview로 scrollView를 올린다.
+        view.addSubview(scrollView)
+        // scrollview를 얹었을때 네비게이션 컨트롤러가 같이 있어서 autolayout이 어긋나서 아래 코드로 수정을 해준다.
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            
+        }
+        
+        // 다음으로 scrollView위에 contentView를 올린다.
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            // $0.height.equalToSuperview() // 이렇게하면 높이는 고정 가로 스크롤만 가능
+            $0.width.equalToSuperview() // 이렇게하면 너비는 고정 세로 스크롤만 가능
+        }
+        
+        // 이제 contentView위에 stackView를 올린다.
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    
+    
 }
